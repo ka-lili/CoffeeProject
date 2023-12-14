5a520997b50779b46d771682aec99ff480f0b35d
lilika67 <l.kayitesi@alustudent.com>
2023-12-14 16:42:47 +0200

    added js

---
+++ about.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".typing-1",{
+    strings: [
+        "Passionate about serving quality coffee", " Hardworking ladies", " Proactive futuristics",
+        "Strong women", "change makers"
+    ],
+    typeSpeed: 80,
+    backSpeed: 60,
+    loop: true
+});
