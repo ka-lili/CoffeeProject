4a8f28eb08dfe817455e1357c0dbf380c62220dc
anysia1 <anysiakamanzi@gmail.com>
2023-12-15 10:01:40 +0200

    contact

--- contact.js
+++ contact.js
@@ -1,19 +1,34 @@
-function validateForm() {
-    var name = document.getElementById('name').value.trim();
-    var email = document.getElementById('email').value.trim();
-    var message = document.getElementById('message').value.trim();
+document.addEventListener("DOMContentLoaded", function () {
+    const contactForm = document.getElementById("contactForm");
 
-    if (name === '' || email === '' || message === '') {
-        alert('Please fill in all fields');
-        return false;
+    contactForm.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        const formData = new FormData(contactForm);
+        
+        const message = formData.get('message');
+        const name = formData.get('name');
+        const email = formData.get('email');
+
+
+        displayConfirmationMessage(name, email, message);
+
+        contactForm.reset();
+    });
+
+    function displayConfirmationMessage(name, email, message) {
+    
+        const confirmationMessage = `Thank you😊, ${name} (${email}), for your message to Nyanyumba Women Coffee Shop! We will get in touch with you soon.\n\nMessage: ${message}`;
+
+        alert(confirmationMessage);
     }
 
-    // Email validation using a simple regular expression
-    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(email)) {
-        alert('Please enter a valid email address');
-        return false;
-    }
+    document.getElementById("profile-link").classList.add("fab", "fa-facebook");
+    document.getElementById("profile-link2").classList.add("fab", "fa-twitter");
+    document.getElementById("profile-link3").classList.add("fab", "fa-instagram");
+    document.getElementById("whatsapp-link").classList.add("fab", "fa-whatsapp");
 
-    return true;
-}
+    document.querySelectorAll("img").forEach(function (img) {
+        img.classList.add("img-responsive");
+    });
+});
