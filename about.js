b0e52b95cd726d9542962304bfce519d74b2fb99
lilika67 <l.kayitesi@alustudent.com>
2023-12-15 09:49:25 +0200

    new commit

--- about.js
+++ about.js
@@ -1,9 +1,9 @@
 var typed = new Typed(".typing-1",{
     strings: [
-        "Passionate about serving quality coffee", " Hardworking ladies", " Proactive futuristics",
-        "Strong women", "change makers"
+        "Passionate about serving quality coffee👏", " Hardworking ladies😘", " Proactive futuristics✨",
+        "Strong women🎉", "change makers🎇"
     ],
     typeSpeed: 80,
     backSpeed: 60,
-    loop: true
+    loop: true,
 });
